d7766fcf057af1933f61bde287763a365e1e74ee
unknown <asiakedir@gmail.com>
2021-12-30 11:26:06 +0300

    working on slider

---
+++ src/data.js
@@ -0,0 +1,24 @@
+export const sliderItems = [
+    {
+        id: 1,
+        title: "Summer sale",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor!",
+        image: "https://picsum.photos/1000/1000",
+        backgroundColor: "fdfaf5"
+    },
+    {
+        id: 2,
+        title: "Winter sale",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor!",
+        image: "https://picsum.photos/1000/1000",
+        backgroundColor: "f5fafd"
+    },
+    {
+        id: 2,
+        title: "Clearance sale",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor!",
+        image: "https://picsum.photos/1000/1000",
+        backgroundColor:"fafdf5"
+    }
+
+]
